b7b33d578034ed4e97ac94ca14cb390c0bcd1ab5
Kozzen <timotiuswinzen@gmail.com>
2023-03-23 23:32:58 +0700

    3rd change

--- src/handler.js
+++ src/handler.js
@@ -81,7 +81,7 @@ const editNoteByIdHandler = (request, h) => {
     };
     const response = h.response({
       status: 'success',
-      message: ' Catatan berhasil diperbaharui',
+      message: 'Catatan berhasil diperbarui',
     });
     response.code(200);
     return response;
